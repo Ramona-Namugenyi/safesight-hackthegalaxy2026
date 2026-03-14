46c345a5237db57031523d58cf1990fddc741d24
Madelina Deng <mad31@sfu.ca>
2026-03-14 13:00:34 -0700

    Backend: load crime CSV and create safety endpoint

---
+++ app.py
@@ -0,0 +1,32 @@
+
+from flask import Flask, request, jsonify
+import pandas as pd
+
+app = Flask(__name__)
+
+# Load crime data
+df = pd.read_csv("data/crime_2025.csv")
+
+df.columns = df.columns.str.upper()
+
+
+@app.route("/")
+def home():
+    return "SafeSight Backend Running"
+
+
+@app.route("/safety")
+def safety():
+
+    neighbourhood = request.args.get("neighbourhood")
+
+    incidents = df[df["NEIGHBOURHOOD"].str.lower() == neighbourhood.lower()]
+
+    return jsonify({
+        "neighbourhood": neighbourhood,
+        "incident_count": len(incidents)
+    })
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
